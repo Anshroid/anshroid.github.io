788bc20b651efb560323fa315bd8e44e9bdba028
anshroid <anshroid07@gmail.com>
2023-02-09 23:12:23 +0000

    Flesh out main page, initial ideas for bg anim

---
+++ index.js
@@ -0,0 +1,15 @@
+document.querySelector("#theme-changer").onclick = () => {
+	let next = document.body.getAttribute('data-color-mode') == 'light' ? 'dark' : 'light';
+
+	document.body.setAttribute('data-color-mode', next);
+	document.body.setAttribute('data-dark-theme', next);
+
+	document.querySelector('#moon').classList.toggle('v-hidden');
+	document.querySelector('#sun').classList.toggle('v-hidden');
+};
+
+let bgImage = document.querySelector("#bg-image")
+document.body.onmousemove = (e) => {
+	bgImage.style.setProperty('--x', -e.clientX * 0.1 + "px");
+	bgImage.style.setProperty('--y', -e.clientY * 0.1 + "px");
+};
