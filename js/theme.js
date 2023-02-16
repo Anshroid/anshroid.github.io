268067f1d11ab06aa7a33082c5331917348d62b6
anshroid <anshroid07@gmail.com>
2023-02-16 18:54:56 +0000

    Theme changer fixes - Add theme changer to sidebar menu - Fix v-hidden instead of d-none - Remove unused theme.css in favour of `fill="currentColor"`

--- js/theme.js
+++ js/theme.js
@@ -1,22 +1,22 @@
-document.querySelector("#theme-changer").onclick = () => {
+document.querySelectorAll(".theme-changer").forEach(elem => {elem.onclick = () => {
 	let next = document.body.getAttribute('data-color-mode') === 'light' ? 'dark' : 'light';
 
 	document.body.setAttribute('data-color-mode', next);
 	document.body.setAttribute('data-dark-theme', next);
 
-	document.querySelector('#moon').classList.toggle('v-hidden');
-	document.querySelector('#sun').classList.toggle('v-hidden');
+	document.querySelectorAll('.moon').forEach(elem => elem.classList.toggle('d-none'));
+	document.querySelectorAll('.sun').forEach(elem => elem.classList.toggle('d-none'));
 
 	window.localStorage.setItem('data-theme', next);
-};
+}});
 
 let theme = window.localStorage.getItem('data-theme');
+console.log(theme)
 if (theme) {
 	document.body.setAttribute('data-color-mode', theme);
 	document.body.setAttribute('data-dark-theme', theme);
-
 	if (theme === 'light') {
-		document.querySelector('#moon').classList.toggle('v-hidden');
-		document.querySelector('#sun').classList.toggle('v-hidden');
+		document.querySelectorAll('.moon').forEach(elem => elem.classList.toggle('d-none'));
+		document.querySelectorAll('.sun').forEach(elem => elem.classList.toggle('d-none'));
 	}
 }
