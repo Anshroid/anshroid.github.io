edd40548cc57aef1ecdf007c2b0a1137bad7b458
anshroid <anshroid07@gmail.com>
2023-04-01 14:14:14 +0100

    Fix broken mouse-responsive background

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,5 @@
 let bgImage = document.querySelector("#bg-image")
 document.body.onmousemove = (e) => {
-	bgImage.animate([
-		{ "--x": -e.clientX * 0.08 + "px" },
-		{ "--y": -e.clientX * 0.1 + "px" },
-	], 1000)
+	bgImage.style.setProperty("--x", -e.clientX * 0.08 + "px");
+	bgImage.style.setProperty("--y", -e.clientX * 0.1 + "px");
 };
