b23199703e387b38585d3bc70bd7f588284faa06
Anshroid Nemo <29677325+Anshroid@users.noreply.github.com>
2023-04-06 21:17:13 +0100

    Update index.js

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,5 @@
 let bgImage = document.querySelector("#bg-image")
 document.body.onmousemove = (e) => {
 	bgImage.style.setProperty("--x", -e.clientX * 0.08 + "px");
-	bgImage.style.setProperty("--y", -e.clientX * 0.1 + "px");
+	bgImage.style.setProperty("--y", -e.clientY * 0.1 + "px");
 };
