d59f5ae809a7cef091a8faa69961572b40161919
Anshroid Nemo <anshroid07@gmail.com>
2023-08-23 10:05:50 +0100

    Updates: - Finally fixed the smooth background movement issue - Update about page

--- static/js/index.js
+++ static/js/index.js
@@ -1,5 +1,15 @@
 let bgImage = document.querySelector("#bg-image")
+
+let stopMovingInterval;
 document.body.onmousemove = (e) => {
-	bgImage.style.setProperty("--x", -e.clientX * 0.08 + "px");
-	bgImage.style.setProperty("--y", -e.clientY * 0.1 + "px");
+    clearInterval(stopMovingInterval);
+    bgImage.animate([{
+        backgroundPositionX: -e.clientX * 0.08 + "px",
+        backgroundPositionY: -e.clientY * 0.10 + "px"
+    }], 500);
+
+    stopMovingInterval = setInterval(() => {
+        bgImage.style.setProperty("background-position-x", -e.clientX * 0.08 + "px");
+        bgImage.style.setProperty("background-position-y", -e.clientY * 0.10 + "px");
+    }, 400)
 };
